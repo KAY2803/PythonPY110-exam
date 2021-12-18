d145492021aef29d50911f664e1621e7ac7cce9f
Anna Kim <kimspb85@mail.ru>
2021-12-18 13:44:21 +0300

    modify main_2

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import random
 import conf
 
 
-def generate_books(pk=1):
+def generate_fields():
     with open('book.txt', 'r', encoding='utf8') as f:
         name_book = f.read().split('\n')
     fake = Faker()
@@ -21,33 +21,32 @@ def generate_books(pk=1):
     gen_fields = {
                       'Title': random.choice(name_book),
                       'year': random.randrange(1600, 2021),
-                      'pages': random.randrange(10, 500),
+                      'pages': random.randrange(10, 10000),
                       'isbn13': isbn,
-                      'rating': random.uniform(0, 5),
-                      'price': random.uniform(10, 10000),
+                      'rating': float(f'{random.uniform(0, 5):.2f}'),
+                      'price': float(f'{random.uniform(10, 10000):.2f}'),
                       'author': random.sample(author_list, random.randint(1, 3))
-    }
-    final_gen_book = {
-                        'model': conf.MODEL,
-                        'pk': pk,
-                        'fields': gen_fields
     }
 
     yield gen_fields
 
+
 if __name__ == "__main__":
 
-    with open('gen_book', 'w', encoding='utf-8') as file:
-        pk = 1
-        for _ in range(5):
-            books = {
+
+    def gen_books(pk=1):
+        with open('gen_book', 'w', encoding='utf-8') as file:
+            for _ in range(3):
+                books = {
                         'model': conf.MODEL,
                         'pk': pk,
-                        'fields': next(generate_books())
-            }
-            pk += 1
-            json.dump(books, file, ensure_ascii=False, indent=4)
+                        'fields': next(generate_fields())
+                }
+                pk += 1
+                json.dump(books, file, ensure_ascii=False, indent=4)
 
+
+    gen_books()
 
 
 
